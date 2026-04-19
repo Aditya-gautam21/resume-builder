fed9c2ac9d9d8a3f1a10c46ac823e99474e097b2
Aditya-gautam21 <adigaur121@gmail.com>
2026-04-19 15:37:23 +0530

    refactor: finalised data loader for text data

--- backend/data_loader.py
+++ backend/data_loader.py
@@ -4,6 +4,20 @@ from langchain_community.document_loaders import PyPDFLoader
 
 load_dotenv()
 
-loader = PyPDFLoader(
-    file_path=os.getenv('FILE_PATH')
-)
+def load_data():
+    loader = PyPDFLoader(
+        file_path=os.getenv('FILE_PATH')
+    )
+
+    data  = loader.load()
+
+    data_txt = " ".join(chunk.page_content for chunk in data)
+
+    '''
+    #If the data is to be stored in txt file 
+
+    with open('resume.txt', 'a', encoding='utf-8') as f:
+        f.write(data_txt)
+    '''
+
+    return data_txt
