615d031a7a35022f9389da84450a3dbddf78062c
Aditya-gautam21 <adigaur121@gmail.com>
2026-04-22 17:49:18 +0530

    feat: created proper file structure for better maintenance

---
+++ backend/services/parser.py
@@ -0,0 +1,48 @@
+import os 
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from llama_cpp import Llama
+
+from backend.services.data_loader import load_data
+from backend.utils.prompts import Prompts
+
+load_dotenv()
+
+resume_data = load_data()
+
+# Extraction using local llm model(currently using gemma e4b)
+llm = Llama(
+    model_path=os.getenv('LOCAL_MODEL_PATH'),
+    n_gpu_layers=8,
+    n_ctx=2048,
+    verbose=False
+    )
+
+'''
+# Openai llm extraction through api
+
+llm = ChatOpenAI(
+    model="gpt-4o-mini",
+    api_key=os.getenv('OPENAI_API_KEY'),
+    temperature=0
+)
+
+'''
+
+
+prompt = Prompts.resume_extraction_prompt(resume_data)
+
+response = llm.create_chat_completion(
+    messages=[
+        {
+            'role':'system',
+            'content': 'You extract structured JSON from resumes. Return ONLY valid JSON.'
+        },
+        {
+            'role': 'user',
+            'content': prompt
+        }
+    ],
+    temperature=0
+)
+print(response['choices'][0]['message']['content'])
