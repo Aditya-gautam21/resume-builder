68471794c18fc290c8f309d6ce52bb4a3d64f8ff
Aditya-gautam21 <adigaur121@gmail.com>
2026-04-26 17:53:07 +0530

    minor changes

--- backend/services/parser.py
+++ backend/services/parser.py
@@ -8,41 +8,46 @@ from backend.utils.prompts import Prompts
 
 load_dotenv()
 
-resume_data = load_data()
+def json_parser():
+    resume_data = load_data()
 
-# Extraction using local llm model(currently using gemma e4b)
-llm = Llama(
-    model_path=os.getenv('LOCAL_MODEL_PATH'),
-    n_gpu_layers=8,
-    n_ctx=2048,
-    verbose=False
+    # Extraction using local llm model(currently using gemma e4b)
+    llm = Llama(
+        model_path=os.getenv('LOCAL_MODEL_PATH'),
+        n_gpu_layers=8,
+        n_ctx=2048,
+        verbose=False
+        )
+
+    '''
+    # Openai llm extraction through api
+
+    llm = ChatOpenAI(
+        model="gpt-4o-mini",
+        api_key=os.getenv('OPENAI_API_KEY'),
+        temperature=0
     )
 
-'''
-# Openai llm extraction through api
-
-llm = ChatOpenAI(
-    model="gpt-4o-mini",
-    api_key=os.getenv('OPENAI_API_KEY'),
-    temperature=0
-)
-
-'''
+    '''
 
 
-prompt = Prompts.resume_extraction_prompt(resume_data)
+    prompt = Prompts.resume_extraction_prompt(resume_data)
 
-response = llm.create_chat_completion(
-    messages=[
-        {
-            'role':'system',
-            'content': 'You extract structured JSON from resumes. Return ONLY valid JSON.'
-        },
-        {
-            'role': 'user',
-            'content': prompt
-        }
-    ],
-    temperature=0
-)
-print(response['choices'][0]['message']['content'])
+    response = llm.create_chat_completion(
+        messages=[
+            {
+                'role':'system',
+                'content': 'You extract structured JSON from resumes. Return ONLY valid JSON.'
+            },
+            {
+                'role': 'user',
+                'content': prompt
+            }
+        ],
+        temperature=0
+    )
+
+    return (response['choices'][0]['message']['content'])
+
+if __name__ == '__main__':
+    print(json_parser())
