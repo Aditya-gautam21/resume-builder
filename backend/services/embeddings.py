280894b6cdd00e73ab733c8d178cd7c0c6e0853c
Aditya-gautam21 <adigaur121@gmail.com>
2026-04-28 13:41:59 +0530

    refactor: emdedding and vectorstore for json data

--- backend/services/embeddings.py
+++ backend/services/embeddings.py
@@ -1,11 +1,18 @@
 import os
+from langchain_core.documents import Document
 from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-import faiss 
 
-from backend.services.parser import json_parser
+from backend.services.json_chunk import json_chunking
 
-chunks = json_parser()
+chunks = json_chunking()
+
+documents = [
+    Document(
+        page_content=chunk["content"],
+        metadata=chunk["metadata"]
+    ) for chunk in chunks
+]
 
 embeddings = OpenAIEmbeddings(model='text-embedding-3-small')
 vector_store = FAISS.from_documents(chunks, embeddings)
