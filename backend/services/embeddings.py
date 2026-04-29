9ca19bed47acf82d764d561955ccec2246b22d2e
Aditya-gautam21 <adigaur121@gmail.com>
2026-04-29 20:38:50 +0530

    refactor: separating create and load vectorstore functions

--- backend/services/embeddings.py
+++ backend/services/embeddings.py
@@ -1,20 +1,32 @@
 import os
-from langchain_core.documents import Document
+from pathlib import Path
 from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
+from backend.services.json_loader import chunker
 
-from backend.services.json_chunk import json_chunking
+INDEX_PATH = Path('/home/adityagautam/Desktop/Projects/resume-builder/backend/data/vectorstores')
 
-chunks = json_chunking()
+def create_vector_store():
+    chunks = chunker()
 
-documents = [
-    Document(
-        page_content=chunk["content"],
-        metadata=chunk["metadata"]
-    ) for chunk in chunks
-]
+    embeddings = OpenAIEmbeddings(model='text-embedding-3-small')
+    vector_store = FAISS.from_documents(chunks, embeddings)
 
-embeddings = OpenAIEmbeddings(model='text-embedding-3-small')
-vector_store = FAISS.from_documents(chunks, embeddings)
+    vector_store.save_local('INDEX_PATH')
 
-print(vector_store.index_to_docstore_id)
+    return vector_store
+
+def load_vector_store():
+    embeddings = OpenAIEmbeddings(model='text-embedding-3-small')
+
+    if not os.path.exists(INDEX_PATH):
+        create_vector_store()
+
+    else:
+        vector_store = FAISS.load_local(
+            folder_path=INDEX_PATH,
+            embeddings=embeddings,
+            allow_dangerous_deserialization=True
+        )
+
+    return vector_store
