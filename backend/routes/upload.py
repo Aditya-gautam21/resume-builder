6ced16944a5c301952276e47be68293192198711
Aditya-gautam21 <adigaur121@gmail.com>
2026-05-10 21:38:16 +0530

    feat: fastapi endpoint for uploading pdf

--- backend/routes/upload.py
+++ backend/routes/upload.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException, APIRouter
-import shutil
+from pypdf import PdfReader
 from pathlib import Path
-
-from backend.services.data_loader import load_data
-from backend.services.parser import json_parser
-from backend.services.json_loader import chunker
-from backend.services.embeddings import create_vector_store, load_vector_store
+import io
 
 router = APIRouter()
 
-@router.post("/upload-pdf/")
-def upload_resume():
-    resume = load_data(     )
+@router.get("/upload-pdf/")
+async def upload_resume(file: UploadFile = File(...)):
+    if not file.filename.endswith('pdf'):
+        raise HTTPException(status_code=400, detail="Only PDFs are accepted!")
+    
+    contents = await file.read()
+    reader = PdfReader(io.BytesIO(contents))
+
+    text = "\n".join(page.extract_text() or "" for page in reader)
+
+    return text
