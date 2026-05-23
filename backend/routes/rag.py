fa0eefccbf7f67c46fc829a0643784c2408ec6f3
Aditya-gautam21 <adigaur121@gmail.com>
2026-05-23 08:52:29 +0530

    feat: FastAPI route for RAG system

--- backend/routes/rag.py
+++ backend/routes/rag.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter
 
-from backend.services.parser import json_parser
-from backend.services.json_loader import chunker
+from backend.services.json_parser import json_parser
+from backend.services.json_chunker import chunker
 from backend.services.embeddings import create_vector_store, load_vector_store
-from routes.upload import upload_resume
+from backend.routes.generator import upload_resume
 
 router = APIRouter()
 
