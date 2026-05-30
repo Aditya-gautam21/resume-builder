62c5f6a751a9d6c9b6f22204f69653e967f68c05
Aditya-gautam21 <adigaur121@gmail.com>
2026-05-30 20:18:44 +0530

    feat: integrated pdf generation through latex into FastAPI router

--- backend/routes/generator.py
+++ backend/routes/generator.py
@@ -2,20 +2,22 @@ from fastapi import UploadFile, HTTPException, APIRouter, File, Form
 from fastapi.responses import Response
 
 from backend.services.data_loader import extract_text_from_pdf
-from backend.services.json_parser import parse_json
-from backend.services.resume_generation import generate_resume
-from backend.services.pdf_renderer import render_resume_pdf
+from backend.services.json_parser import parse_json_deepseek
+from backend.services.resume_generation import generate_resume_deepseek
+from backend.services.latex_renderer import render_latex
 
 router = APIRouter()
 
 
 @router.post("/parse-resume/")
-async def parse_uploaded_resume(file: UploadFile = File(...)):
+async def parse_uploaded_resume(file: UploadFile = File(...), jd: str = Form(...), pages: int = 1):
     try:
         contents = await file.read()
         resume_text = extract_text_from_pdf(contents, file.filename)
-        resume_json = parse_json(resume_text)
-        return {"status": "ok", "json": resume_json}
+        resume_json = parse_json_deepseek(resume_text)
+        tailored = generate_resume_deepseek(resume_data=resume_json, job_description=jd, pages=pages)
+
+        return {"status": "ok", "latex": tailored}
 
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
@@ -34,15 +36,15 @@ async def tailor_resume(
     resume: UploadFile = File(...),
     jd: str = Form(...),
     pages: int = Form(default=1),
-    template_name: str = Form(default="classic"),
 ):
     try:
         contents = await resume.read()
         resume_txt = extract_text_from_pdf(contents, resume.filename)
-        resume_json = parse_json(resume_txt)
+        resume_json = parse_json_deepseek(resume_txt)
 
-        tailored = generate_resume(resume_data=resume_json, job_description=jd, pages=pages)
-        pdf_bytes = bytes(render_resume_pdf(tailored["tailored_resume"], template_name=template_name))
+        latex_str = generate_resume_deepseek(resume_data=resume_json, job_description=jd, pages=pages)
+
+        pdf_bytes = render_latex(latex_str)
 
         return Response(
             content=pdf_bytes,
@@ -50,5 +52,7 @@ async def tailor_resume(
             headers={"Content-Disposition": "attachment; filename=tailored_resume.pdf"},
         )
 
-    except ValueError as e:
-        raise HTTPException(status_code=400, detail=e)
+    except Exception as e:
+        import traceback
+        traceback.print_exc()
+        raise HTTPException(status_code=400, detail=str(e))
