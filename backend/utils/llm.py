64853e4afeed8c7dbd65aaabe575e5c5694d2383
Aditya-gautam21 <adigaur121@gmail.com>
2026-05-30 20:15:57 +0530

    feat: added lazy imports for every llm

--- backend/utils/llm.py
+++ backend/utils/llm.py
@@ -1,8 +1,5 @@
 import os
 from dotenv import load_dotenv
-from llama_cpp import Llama
-from langchain_openai import ChatOpenAI
-from langchain_deepseek import ChatDeepSeek
 
 load_dotenv()
 
@@ -10,11 +7,14 @@ _local_llm = None
 _openai_llm = None
 _deepseek_llm = None
 
+
 def load_local_llm():
     global _local_llm
     if _local_llm is None:
+        from llama_cpp import Llama 
+
         _local_llm = Llama(
-            model_path=os.getenv('LOCAL_MODEL_PATH'),
+            model_path=os.getenv("LOCAL_MODEL_PATH"),
             n_gpu_layers=12,
             n_ctx=4096,
             verbose=False,
@@ -25,20 +25,25 @@ def load_local_llm():
 def load_openai_llm():
     global _openai_llm
     if _openai_llm is None:
+        from langchain_openai import ChatOpenAI  
+
         _openai_llm = ChatOpenAI(
             model="gpt-4o-mini",
-            api_key=os.getenv('OPENAI_API_KEY'),
+            api_key=os.getenv("OPENAI_API_KEY"),
             temperature=0,
         )
     return _openai_llm
 
+
 def load_deepseek_llm():
     global _deepseek_llm
     if _deepseek_llm is None:
+        from langchain_deepseek import ChatDeepSeek  
+
         _deepseek_llm = ChatDeepSeek(
-            api_key=os.getenv('DEEPSEEK_API_KEY'),
-            model='deepseek-v4-flash',
-            temperature=.21,
-            verbose=False
-            )
+            api_key=os.getenv("DEEPSEEK_API_KEY"),
+            model="deepseek-v4-flash",
+            temperature=0.21,
+            verbose=False,
+        )
     return _deepseek_llm
